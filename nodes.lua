dc813608d2085cfe45a2944d892281e95d1d86fc
David Lynch <kemayo@gmail.com>
2016-11-29 13:39:21 -0600

    Update the rest of the portals with appropriate hide-befores

--- nodes.lua
+++ nodes.lua
@@ -15,25 +15,26 @@ ns.points = {
     },
     --]]
     ["Suramar"] = {
-        [30801090] = { quest=43808, label="Moon Guard Stronghold", },
-        [22903580] = { quest=42230, label="Falanaar", },
-        [42203540] = { quest=43809, label="Tel'anor", },
-        [36204710] = { quest=40956, label="Ruins of Elune'eth", },
-        [43406070] = { quest=43813, label="Sanctum of Order", },
-        [43607910] = { quest=43811, label="Lunastre Estate", },
-        [35808210] = { quest=41575, label="Felsoul Hold", },
-        [47508200] = { quest=42487, label="Waning Crescent", hide_after=43569, },
-        [52007800] = { quest=42889, label="Evermoon Terrace", hide_before=43569, },
-        [64006040] = { quest=44084, label="Twilight Vineyards", },
+        [36204710] = { quest=40956, label="Ruins of Elune'eth", hide_before=40956, }, -- storyline: Survey Says...
+        [22903580] = { quest=42230, label="Falanaar", hide_before=42228, }, -- storyline: Valewalker's Burden, hidden until Hidden City
+        [47508200] = { quest=42487, label="Waning Crescent", hide_after=43569, hide_before=42486, }, -- storyline: Friends on the Outside, hidden until Little One Lost, hidden after Arluin's Request
+        [64006040] = { quest=44084, label="Twilight Vineyards", hide_before=42838, }, -- storyline: Vengeance for Margaux, hidden until Reversal
+        [52007800] = { quest=42889, label="Evermoon Terrace", hide_before=43569, }, -- storyline: The Way Back Home, hidden until 38694
         [54496943] = { quest=44740, label="Astravar Harbor", hide_before=44738, }, -- storyline: Staging Point, hidden until Full Might of the Elves
+        -- These ones are general-access after Ruins is opened:
+        [30801090] = { quest=43808, label="Moon Guard Stronghold", hide_before=40956, },
+        [42203540] = { quest=43809, label="Tel'anor", hide_before=40956, },
+        [43406070] = { quest=43813, label="Sanctum of Order", hide_before=40956, },
+        [43607910] = { quest=43811, label="Lunastre Estate", hide_before=40956, },
+        [35808210] = { quest=41575, label="Felsoul Hold", hide_before=40956, },
         -- entrances
-        [27802230] = { quest=43808, entrance=true, label="Moon Guard (entrance)" },
-        [42606170] = { quest=43813, entrance=true, label="Sanctum of Order (entrance)" },
+        [27802230] = { quest=43808, entrance=true, label="Moon Guard (entrance)", hide_before=40956, },
+        [42606170] = { quest=43813, entrance=true, label="Sanctum of Order (entrance)", hide_before=40956, },
     },
     ["FalanaarTunnels"] = { -- Fal'adore
-        [40901350] = { quest=42230, label="Falanaar", level=32 },
+        [40901350] = { quest=42230, label="Falanaar", level=32, hide_before=42228, },
     },
     ["SuramarLegionScar"] = { -- The Fel Breach
-        [53403680] = { quest=41575, label="Felsoul Hold", },
+        [53403680] = { quest=41575, label="Felsoul Hold", hide_before=40956, },
     },
 }
